36206d2063aed88c3599e18b261a5100ffefae00
RyanShereda <39539590+RyanShereda@users.noreply.github.com>
2020-05-08 10:01:06 -0500

    Adds base project for Week 3 group activity

---
+++ Week3GroupDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Week3GroupDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
