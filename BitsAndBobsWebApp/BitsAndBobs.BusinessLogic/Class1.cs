7b3ae30621d93638955a41bc9321bf4f43da0774
RyanShereda <39539590+RyanShereda@users.noreply.github.com>
2020-05-15 14:43:25 -0500

    Adds initial setup of Project 1.

--- BitsAndBobsWebApp/BitsAndBobs.BusinessLogic/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BitsAndBobs.BusinessLogic
-{
-    public class Class1
-    {
-    }
-}
---
+++ BitsAndBobsWebApp/BitsAndBobs.Data/BitsAndBobsContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitsAndBobs.Data
+{
+    public class BitsAndBobsContext : DbContext
+    {
+        public BitsAndBobsContext (DbContextOptions<BitsAndBobsContext> options) : base(options)
+        {
+
+        }
+
+        public BitsAndBobsContext() { }
+
+        //DbSets go here
+        #region DbSets
+
+        #endregion
+    }
+}
